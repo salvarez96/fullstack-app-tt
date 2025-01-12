5a8f5ab3c945b6b50d7e3f2d104e0f1b186140f2
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-11 22:55:05 -0500

    ✨ Create products API paths

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::controller(ProductController::class)->prefix('products')->group(function () {
+        Route::get('/', 'show');
+        Route::get('/{id}', 'showProduct');
+        Route::post('/', 'create');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'destroy');
+    });
+});
+
+// Route::get('/products', [ProductController::class, 'getProducts']);
