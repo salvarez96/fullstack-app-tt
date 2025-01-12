b41e46971ece98ddd5bd1ebced64631e6bcb7ac3
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-12 16:43:55 -0500

    ✨ Implement products and products cards in products page

---
+++ resources/js/vue/components/products/ProductCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="product-card">
+        <h3>{{ productName }}</h3>
+        <h5>{{ productCategory }}</h5>
+        <img class="product-image mb-2" :src="productImage" :alt="productName" />
+        <h6>Precio: ${{ productPrice }}</h6>
+        <p>{{ productDescription }}</p>
+        <button class="btn btn-primary">A button</button>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        productName: String,
+        productCategory: String,
+        productImage: String,
+        productPrice: String,
+        productDescription: String
+    })
+</script>
+
+<style lang="scss">
+    .product-card {
+        max-width: 300px;
+        margin: 0 auto;
+        border: 1px solid black;
+        border-radius: 20px;
+        padding: 30px 20px;
+    }
+
+    .product-image {
+        min-width: 258px;
+        min-height: 193px;
+        border-radius: 5px;
+    }
+</style>
--- resources/js/vue/pages/ProductsPage.vue
+++ resources/js/vue/pages/ProductsPage.vue
@@ -1,9 +1,46 @@
 <template>
-    <div>
-        <h1>Products in Vue</h1>
+    <div class="main-container container">
+        <h1 class="text-center">Nuestros productos</h1>
+        <div class="products-container">
+            <ProductCard v-for="(product, index) in products.data"
+                :key="index"
+                :productName="product.name"
+                :productCategory="product.category_id"
+                :productImage="product.image"
+                :productPrice="product.price"
+                :productDescription="product.description"
+            />
+        </div>
     </div>
 </template>
 
 <script setup>
+import { onBeforeMount, computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+import ProductCard from '@components/products/ProductCard.vue';
 
+const store = useStore();
+const products = computed(() => store.state.products);
+
+onBeforeMount(() => {
+    store.dispatch('getProducts');
+});
+
+onMounted(() => {
+    console.log(products.value);
+})
 </script>
+
+<style lang="scss">
+    .main-container {
+        max-width: 1100px;
+        margin-top: 50px;
+    }
+
+    .products-container {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 20px;
+        margin-top: 50px;
+    }
+</style>
