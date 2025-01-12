d0ec29cca9a782e4b7700397a5173b66119aa188
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-11 22:47:18 -0500

    🗃️ Create products model and table migration

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+        'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
