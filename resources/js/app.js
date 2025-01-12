f65423ac150f8725d4b52b2e2c1b1b171ebb09f5
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-12 16:41:46 -0500

    💄 Create App default vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,12 @@
-import './bootstrap';
+// import './bootstrap';
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
 import { createApp } from 'vue';
-import ProductsPage from './vue/pages/ProductsPage.vue';
+import ProductsPage from '@pages/ProductsPage.vue';
+import productStore from '@stores/productStore';
+import App from './vue/App.vue';
 
-createApp(ProductsPage).mount('#app');
+const app = createApp(ProductsPage);
+app.component('products-page', ProductsPage);
+app.use(productStore);
+app.mount('#app');
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
-</head>
-<body>
-    <h1>Productos</h1>
-    <div id="app">
-        <ProductsPage />
-    </div>
-</body>
-</html>
+@extends('layouts.app')
+@section('title', 'Products')
+
+@section('content')
+    <products-page />
+@endsection
