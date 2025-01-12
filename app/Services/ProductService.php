9f813437b1ca9213d49ac9eb6c7fce888c412afb
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-11 22:56:20 -0500

    ✨ Create products service
    Add methods to show, create and update products

---
+++ app/Services/ProductService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class ProductService {
+    protected $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function getProducts()
+    {
+        return $this->product->with('category')->paginate(10);
+    }
+
+    public function getProduct(int $id)
+    {
+        return $this->product->with('category')->find($id);
+    }
+
+    public function createProduct(Product $data)
+    {
+        return $this->product->create([
+            'name' => $data->name,
+            'description' => $data->description,
+            'price' => $data->price,
+            'image' => $this->getExternalImage(),
+            'category_id' => $data->category_id,
+        ]);
+    }
+
+    public function updateProduct(Request $request, Product $product)
+    {
+        $updatedProduct = [...$product->toArray(), ...$request->all()];
+        return $product->update($updatedProduct);
+    }
+
+    public function getExternalImage() {
+        $randomNumber = rand(1, 5);
+        $url = "https://picsum.photos/v2/list?page={$randomNumber}";
+        $client = new Client();
+
+        $responseBody = $client->get($url)->getBody()->getContents();
+        $imageUrl = json_decode($responseBody)[rand(0, 30)]->download_url;
+
+        return $imageUrl;
+    }
+}
