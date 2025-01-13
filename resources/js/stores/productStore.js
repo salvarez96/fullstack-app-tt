d316772ae41dde61bd0e72a1a50bde9925349ba8
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-12 20:27:38 -0500

    ✨ Add createProduct method in product store

--- resources/js/stores/productStore.js
+++ resources/js/stores/productStore.js
@@ -5,19 +5,44 @@ import axios from 'axios'
 const productStore = createStore({
     state () {
         return {
-            products: []
+            products: [],
+            categories: []
         }
     },
     mutations: {
         setProducts(state, products) {
             state.products = products
-        }
+        },
+        setCategories(state, categories) {
+            state.categories = categories
+        },
+        // addNewProduct(state, product) {
+        //     state.products.data.push(product)
+        // }
     },
     actions: {
         getProducts({ commit }) {
             axios.get(`/api/v1/products`)
                 .then(response => {
                     commit('setProducts', response.data)
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+        },
+        getCategories({ commit }) {
+            axios.get(`/api/v1/categories`)
+                .then(response => {
+                    commit('setCategories', response.data)
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+        },
+        createProduct({ dispatch }, product) {
+            axios.post(`/api/v1/products`, product)
+                .then(() => {
+                    dispatch('getProducts')
                 })
                 .catch(error => {
                     console.error(error)
