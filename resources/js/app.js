362f7f0393935f4d82e25a7565cf297277f1ff95
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-13 20:34:29 -0500

    ➕ Install element-ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,11 @@ import { createApp } from 'vue';
 import ProductsPage from '@pages/ProductsPage.vue';
 import productStore from '@stores/productStore';
 import App from './vue/App.vue';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(ProductsPage);
 app.component('products-page', ProductsPage);
 app.use(productStore);
+app.use(ElementPlus);
 app.mount('#app');
