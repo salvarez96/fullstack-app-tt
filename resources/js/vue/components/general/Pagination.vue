d9bc21ec460ff924c311e2cc0c024e9c6bb472ba
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-01-13 21:42:11 -0500

    🚸 Add visual aids to pagination component
    Show the current pagination page. Disable next and back buttons in pagination in case there's no more pages to go. Catch and notify errors.

--- resources/js/vue/components/general/Pagination.vue
+++ resources/js/vue/components/general/Pagination.vue
@@ -1,15 +1,15 @@
 <template>
     <nav class="navigation-container" aria-label="Page navigation example">
         <ul class="pagination">
-            <li class="page-item">
+            <li :class="{'page-item': true, 'disabled': !props.links?.prev}">
                 <button class="page-link" @click="handlePagination('previous')" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                 </button>
             </li>
-            <li class="page-item" v-for="link in meta?.links" :key="link.label">
+            <li :class="{'page-item': true, 'active': link.label == currentPage}" v-for="link in meta?.links" :key="link.label">
                 <button class="page-link" v-if="Number(link.label)" @click="handlePagination(link.label)">{{ link.label }}</button>
             </li>
-            <li class="page-item">
+            <li :class="{'page-item': true, 'disabled': !props.links?.next}">
                 <button class="page-link" @click="handlePagination('next')" aria-label="Next">
                     <span aria-hidden="true">&raquo;</span>
                 </button>
@@ -19,10 +19,11 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, onBeforeMount, ref } from 'vue'
+import { onMounted, onUnmounted, onBeforeMount, onUpdated, ref } from 'vue'
 import { useStore } from 'vuex'
 import getCurrentPage from '@helpers/getCurrentPage'
 import handleUrlPagination from '@helpers/handleUrlPagination'
+import { ElNotification } from 'element-plus'
 
 const props = defineProps({
     links: Array,
@@ -37,28 +38,46 @@ onBeforeMount(() => {
     store.dispatch(props.storeAction, {'page': currentPage.value});
 });
 
+onUpdated(() => {
+    setTimeout(() => {
+        currentPage.value = getCurrentPage()
+    }, 100)
+})
 
-function handlePagination(pageToGo, isPopState = false) {
+
+async function handlePagination(pageToGo, isPopState = false) {
     // calculate previous and next page depending on the content's available navigation links
-    if (pageToGo === 'previous' && props.links?.prev) {
-        pageToGo = props.meta.current_page - 1
-    } else if (pageToGo === 'next' && props.links?.next) {
-        pageToGo = props.meta.current_page + 1
-    }
+    try {
+        if (pageToGo === 'previous' && props.links?.prev) {
+            pageToGo = props.meta.current_page - 1
+        } else if (pageToGo === 'next' && props.links?.next) {
+            pageToGo = props.meta.current_page + 1
+        }
 
-    // if the navigation is done through the pagination buttons, push the new page
-    // to the browser's history as a param and then load the content for that page
-    if (Number(pageToGo) && !isPopState) {
-        handleUrlPagination(pageToGo)
+        currentPage.value = pageToGo;
 
-        store.dispatch(props.storeAction, {'page': pageToGo});
-    }
+        // if the navigation is done through the pagination buttons, push the new page
+        // to the browser's history as a param and then load the content for that page
+        if (Number(pageToGo) && !isPopState) {
+            handleUrlPagination(pageToGo)
 
-    // if navigation is done through the browser's back and forward buttons,
-    // set the current page to the one in the URL and then load the content for that page
-    if (isPopState) {
-        currentPage.value = getCurrentPage();
-        store.dispatch(props.storeAction, {'page': currentPage.value});
+            await store.dispatch(props.storeAction, {'page': pageToGo});
+        }
+
+        // if navigation is done through the browser's back and forward buttons,
+        // set the current page to the one in the URL and then load the content for that page
+        if (isPopState) {
+            currentPage.value = getCurrentPage();
+            await store.dispatch(props.storeAction, {'page': currentPage.value});
+        }
+    } catch (error) {
+        console.error(error)
+
+        ElNotification({
+            title: 'Error',
+            message: 'Es probable que se haya navegado hacia una página que no existe para la cantidad de productos que se están mostrando actualmente.',
+            type: 'error'
+        })
     }
 }
 
